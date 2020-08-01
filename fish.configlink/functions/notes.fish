213e471769cce4a958610df2b17c330740da3e0d
Noah Masur <noah.masur@take2games.com>
2020-07-31 21:00:49 -0400

    migrate journal to main dir

--- fish.configlink/functions/notes.fish
+++ fish.configlink/functions/notes.fish
@@ -4,28 +4,28 @@ function notes --description "Notes functions"
 
     function journal --description "Create today's journal"
         set today (date -j +"%Y-%m-%d_%a")
-        set today_journal $HOME/Documents/notes/journal/$today.md
+        set today_journal $HOME/Documents/notes/$today.md
         if [ -f $today_journal ]
             echo "Already exists."
         else
             set yesterday (date -jv "-1d" +"%Y-%m-%d_%a")
             set tomorrow (date -jv "+1d" +"%Y-%m-%d_%a")
             set weather (curl "https://wttr.in/?format=1")
-            printf "[[journal/$yesterday|Previous]] - [[calendar|Index]] - [[journal/$tomorrow|Next]]\n\n---\n\n$weather\n\n # Tasks\n\n\n# Log\n\n\n# Communication\n\n---\n\n# Meetings\n\n" > $today_journal
+            printf "[[$yesterday|Previous]] - [[calendar|Index]] - [[$tomorrow|Next]]\n\n---\n\n$weather\n\n # Tasks\n\n\n# Log\n\n\n# Communication\n\n---\n\n# Meetings\n\n" > $today_journal
             echo "New journal added."
         end
     end
 
     function today --description "Open today's journal"
         set today (date -j +"%Y-%m-%d_%a")
-        set today_journal $HOME/Documents/notes/journal/$today.md
+        set today_journal $HOME/Documents/notes/$today.md
         if [ -f $today_journal ]
             vim $today_journal
         else
             set yesterday (date -jv "-1d" +"%Y-%m-%d_%a")
             set tomorrow (date -jv "+1d" +"%Y-%m-%d_%a")
             set weather (curl "https://wttr.in/?format=1")
-            printf "[[journal/$yesterday|Previous]] - [[calendar|Index]] - [[journal/$tomorrow|Next]]\n\n---\n\n$weather\n\n # Tasks\n\n\n# Log\n\n\n# Communication\n\n---\n\n# Meetings\n\n" > $today_journal
+            printf "[[$yesterday|Previous]] - [[calendar|Index]] - [[$tomorrow|Next]]\n\n---\n\n$weather\n\n # Tasks\n\n\n# Log\n\n\n# Communication\n\n---\n\n# Meetings\n\n" > $today_journal
             echo "New journal added."
             vim $today_journal
         end
