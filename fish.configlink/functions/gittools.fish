dfc02156a4f1efcefab7e3a5ce9e034b52153353
Noah Masur <noah.masur@take2games.com>
2020-11-28 16:10:50 -0500

    colorize git add fzf diff previews

--- fish.configlink/functions/gittools.fish
+++ fish.configlink/functions/gittools.fish
@@ -42,7 +42,7 @@ function gittools
                 --height 50% \
                 -m \
                 --preview-window right:70% \
-                --preview 'set -l IFS; set gd (git diff --color=always (echo {} | awk \'{$1=$1};1\' | cut -d" " -f2)); if test "$gd"; echo "$gd"; else; bat (echo {} | awk \'{$1=$1};1\' | cut -d" " -f2); end')
+                --preview 'set -l IFS; set gd (git diff --color=always (echo {} | awk \'{$1=$1};1\' | cut -d" " -f2)); if test "$gd"; echo "$gd"; else; bat --color=always (echo {} | awk \'{$1=$1};1\' | cut -d" " -f2); end')
         and for gf in $gitfile
             set gf (echo $gf \
                 | awk '{$1=$1};1' \
