e616d30fb32b498190bb9c398739b1a2c3e70b73
Noah Masur <7386960+nmasur@users.noreply.github.com>
2021-05-05 08:32:39 -0400

    autosuggestion keybinding

--- fish.configlink/functions/fish_user_key_bindings.fish
+++ fish.configlink/functions/fish_user_key_bindings.fish
@@ -4,4 +4,5 @@ function fish_user_key_bindings
     bind -M insert \co 'edit'
     bind -M insert \ce 'recent'
     bind -M insert \cg 'commandline-git-commits'
+    bind -M insert \x1F accept-autosuggestion
 end
