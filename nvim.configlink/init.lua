81815ed55101427513d9a692abdac96ebe14de12
Noah Masur <7386960+nmasur@users.noreply.github.com>
2021-11-09 08:41:28 -0500

    telescope keymap hints

--- nvim.configlink/init.lua
+++ nvim.configlink/init.lua
@@ -350,7 +350,10 @@ local actions = require("telescope.actions")
 require("telescope").setup({
     defaults = {
         mappings = {
-            i = { ["<esc>"] = actions.close, },
+            i = {
+                ["<esc>"] = actions.close,
+                ["<C-h>"] = "which_key",
+            },
         },
     },
     pickers = {
