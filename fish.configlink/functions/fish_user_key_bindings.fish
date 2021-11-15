b9747c4e6f0d6d45af11d10947167b120f312b2b
Noah Masur <7386960+nmasur@users.noreply.github.com>
2021-11-15 11:17:34 -0500

    fish keybind updates

--- fish.configlink/functions/fish_user_key_bindings.fish
+++ fish.configlink/functions/fish_user_key_bindings.fish
@@ -2,10 +2,14 @@
 
 function fish_user_key_bindings
     bind -M insert \co 'edit'
-    bind -M insert \ca 'cd; and edit; cd -'
+    bind -M default \co 'edit'
+    bind -M insert \ca 'cd ~; and edit; and commandline -a "; cd -"; commandline -f execute'
+    bind -M default \ca 'cd ~; and edit; and commandline -a "; cd -"; commandline -f execute'
     bind -M insert \ce 'recent'
+    bind -M default \ce 'recent'
     bind -M insert \cg 'commandline-git-commits'
     bind -M insert \cf 'fcd'
+    bind -M default \cf 'fcd'
     bind -M insert \cp 'prj'
     bind -M default \cp 'prj'
     bind -M insert \x1F accept-autosuggestion
