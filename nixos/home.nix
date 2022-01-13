cc06fadcd98e0f93c52b1aac9807575f10c2e7b0
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-01-13 18:44:56 -0500

    alacritty colors

--- nixos/home.nix
+++ nixos/home.nix
@@ -29,37 +29,68 @@ in
     gh
   ];
 
-  #programs.alacritty = {
-  #  enable = true;
-  #  settings = {
-  #    window = {
-  #      dimensions = {
-  #        columns = 85;
-  #        lines = 30;
-  #      };
-  #      padding = {
-  #        x = 20;
-  #        y = 20;
-  #      };
-  #    };
-  #    scrolling.history = 10000;
-  #    font = {
-  #      size = 15.0;
-  #    };
-  #    key_bindings = [
-  #      {
-  #        key = "F";
-  #        mods = "Super";
-  #        action = "ToggleFullscreen";
-  #      }
-  #      {
-  #        key = "L";
-  #        mods = "Super";
-  #        chars = "\x1F";
-  #      }
-  #    ];
-  #  };
-  #};
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        dimensions = {
+          columns = 85;
+          lines = 30;
+        };
+        padding = {
+          x = 20;
+          y = 20;
+        };
+      };
+      scrolling.history = 10000;
+      font = {
+        size = 13.0;
+      };
+      key_bindings = [
+        {
+          key = "F";
+          mods = "Super";
+          action = "ToggleFullscreen";
+        }
+        {
+          key = "L";
+          mods = "Super";
+          chars = "\x1F";
+        }
+      ];
+      colors = {
+        primary = {
+          background = "0x1d2021";
+          foreground = "0xd5c4a1";
+        };
+        cursor = {
+          text = "0x1d2021";
+          cursor = "0xd5c4a1";
+        };
+        normal = {
+          black =   "0x1d2021";
+          red =     "0xfb4934";
+          green =   "0xb8bb26";
+          yellow =  "0xfabd2f";
+          blue =    "0x83a598";
+          magenta = "0xd3869b";
+          cyan =    "0x8ec07c";
+          white =   "0xd5c4a1";
+        };
+        bright = {
+          black =   "0x665c54";
+          red =     "0xfe8019";
+          green =   "0x3c3836";
+          yellow =  "0x504945";
+          blue =    "0xbdae93";
+          magenta = "0xebdbb2";
+          cyan =    "0xd65d0e";
+          white =   "0xfbf1c7";
+        };
+      };
+      draw_bold_text_with_bright_colors = false;
+    };
+  };
 
   programs.fish = {
     enable = true;
