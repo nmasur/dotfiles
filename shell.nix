30711aa05ce48db558922701e0f65b2705e71c4c
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-04-23 18:24:46 +0000

    more shell requirements for dotfiles

--- shell.nix
+++ shell.nix
@@ -1,5 +1,10 @@
 # Environment with formatting tools for editing these files
 { pkgs ? import <nixpkgs> { } }:
 pkgs.mkShell {
-  nativeBuildInputs = [ pkgs.buildPackages.stylua pkgs.buildPackages.nixfmt ];
+  nativeBuildInputs = [
+    pkgs.buildPackages.stylua
+    pkgs.buildPackages.nixfmt
+    pkgs.buildPackages.shfmt
+    pkgs.buildPackages.shellcheck
+  ];
 }
