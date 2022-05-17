82e85a7d95bfcfb80d43f5057725037d9a746584
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-05-16 20:27:26 -0400

    fix gpp

--- modules/shell/fish/functions/git-push-upstream.fish
+++ modules/shell/fish/functions/git-push-upstream.fish
@@ -1,4 +1,6 @@
 set -l branch (git branch 2>/dev/null | grep '^\*' | colrm 1 2)
-set -l command "git push --set-upstream origin $branch"
-commandline -r $command
-commandline -f execute
+and set -l command "git push --set-upstream origin $branch"
+and commandline -r $command
+and commandline -f execute
+and echo "git push --set-upstream origin $branch"
+and git push --set-upstream origin $branch
