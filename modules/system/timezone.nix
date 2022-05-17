3b385f21fcad502f484741193e0550c1ea543f3c
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-05-16 20:37:26 -0400

    fix geoclue when enabling wifi

--- modules/system/timezone.nix
+++ modules/system/timezone.nix
@@ -2,6 +2,7 @@
 
   # Service to determine location for time zone
   services.geoclue2.enable = true;
+  services.geoclue2.enableWifi = false; # Breaks when it can't connect
   location = { provider = "geoclue2"; };
 
   # Enable local time based on time zone
