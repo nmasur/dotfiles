99bff6fa422e64a7b12d396ea2063e0d5a1365cf
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-05-19 08:48:09 -0400

    try to prevent keyboard from waking again

--- modules/hardware/sleep.nix
+++ modules/hardware/sleep.nix
@@ -3,6 +3,7 @@
   # Prevent wake from keyboard
   powerManagement.powerDownCommands = ''
     echo disabled > /sys/bus/usb/devices/1-6/power/wakeup
+    echo disabled > /sys/bus/usb/devices/1-8/power/wakeup
   '';
 
 }
