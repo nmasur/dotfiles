2539151df9bc1ec0f797befc4d78f2917ac567b3
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-06-12 16:43:07 -0400

    switch to grub for dual-boot convenience

--- modules/hardware/boot.nix
+++ modules/hardware/boot.nix
@@ -1,8 +1,23 @@
-{ ... }: {
+{ config, ... }: {
 
-  # Use the systemd-boot EFI boot loader.
-  # These came with the system and I don't know if they're required.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    grub = {
+      enable = true;
 
+      # Not sure what this does, but it involves the UEFI/BIOS
+      efiSupport = true;
+
+      # Check for other OSes and make them available
+      useOSProber = true;
+
+      # Install GRUB onto the boot disk
+      # device = config.fileSystems."/boot".device;
+
+      # Don't install GRUB, required for UEFI?
+      device = "nodev";
+    };
+
+    # Allows GRUB to interact with the UEFI/BIOS I guess
+    efi.canTouchEfiVariables = true;
+  };
 }
