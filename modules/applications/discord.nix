404426c480e88abedec9e333443ae932cb14d185
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-06-13 20:29:49 -0400

    prevent discord auto-update

--- modules/applications/discord.nix
+++ modules/applications/discord.nix
@@ -4,6 +4,16 @@
     unfreePackages = [ "discord" ];
     home-manager.users.${config.user} = {
       home.packages = with pkgs; [ discord ];
+      xdg.configFile."discord/settings.json".text = ''
+        {
+          "BACKGROUND_COLOR": "#202225",
+          "IS_MAXIMIZED": false,
+          "IS_MINIMIZED": false,
+          "OPEN_ON_STARTUP": false,
+          "MINIMIZE_TO_TRAY": false,
+          "SKIP_HOST_UPDATE": true
+        }
+      '';
     };
   };
 }
