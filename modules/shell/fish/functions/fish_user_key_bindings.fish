c048b7244fbe9e6f3b8ec6d6203446ba51fbad28
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-06-14 07:12:55 -0400

    use ctrl-shift-g to insert path to git repo

--- modules/shell/fish/functions/fish_user_key_bindings.fish
+++ modules/shell/fish/functions/fish_user_key_bindings.fish
@@ -5,6 +5,7 @@ bind -M default \ca 'cd ~; and edit; and commandline -a "; cd -"; commandline -f
 bind -M insert \ce recent
 bind -M default \ce recent
 bind -M insert \cg commandline-git-commits
+bind -M insert \cG 'commandline -i (git rev-parse --show-toplevel 2>/dev/null || echo ".")'
 bind -M insert \cf fcd
 bind -M default \cf fcd
 bind -M insert \cp projects
