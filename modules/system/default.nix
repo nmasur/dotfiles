bfd3eb997ce1625abdb76b8954b4b8c2f360686a
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-06-26 23:08:45 -0400

    temp: home-manager requires state version

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -1,8 +1,10 @@
-{ ... }: {
+{ config, ... }: {
 
   imports = [ ./user.nix ./timezone.nix ./doas.nix ];
 
   # Pin a state version to prevent warnings
   system.stateVersion = "22.11";
+  home-manager.users.${config.user}.home.stateVersion = "22.11";
+  home-manager.users.root.home.stateVersion = "22.11";
 
 }
