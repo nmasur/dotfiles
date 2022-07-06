9aad4efd27c99ef347409de3ab62984099d48e39
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-07-05 21:05:24 -0400

    fix: recent file lookup with wrong stat

--- modules/shell/fish/functions/recent.fish
+++ modules/shell/fish/functions/recent.fish
@@ -1,4 +1,4 @@
-set vimfile (fd -t f --exec stat -f "%m%t%N" | sort -nr | cut -f2 | fzf)
+set vimfile (fd -t f --exec /usr/bin/stat -f "%m%t%N" | sort -nr | cut -f2 | fzf)
 and set vimfile (echo $vimfile | tr -d '\r')
 and commandline -r "vim $vimfile"
 and commandline -f execute
