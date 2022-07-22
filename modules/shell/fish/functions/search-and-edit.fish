2a4a8efa483f9e8fd826799d5ffe86f03a6f3c04
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-07-22 09:49:21 -0400

    search-and-edit jump to line number

--- modules/shell/fish/functions/search-and-edit.fish
+++ modules/shell/fish/functions/search-and-edit.fish
@@ -15,6 +15,7 @@ set vimfile ( \
         --preview 'bat --color=always {1} --highlight-line {2}' \
         --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
 )
+and set line_number (echo $vimfile | tr -d '\r' | cut -d':' -f2)
 and set vimfile (echo $vimfile | tr -d '\r' | cut -d':' -f1)
-and commandline -r "vim $vimfile"
+and commandline -r "vim +$line_number $vimfile"
 and commandline -f execute
