f38f782b632b55da8b7ea8b19a9f2272c35a3340
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-10-03 12:32:09 +0000

    add helper statements for loadkeys app

--- apps/loadkey.nix
+++ apps/loadkey.nix
@@ -3,7 +3,10 @@
   type = "app";
 
   program = builtins.toString (pkgs.writeShellScript "loadkey" ''
+    printf "\nEnter the seed phrase for your SSH key...\n"
+    printf "\nThen press ^D when complete.\n\n"
     ${pkgs.melt}/bin/melt restore ~/.ssh/id_ed25519
+    printf "\n\nContinuing activation.\n\n"
   '');
 
 }
