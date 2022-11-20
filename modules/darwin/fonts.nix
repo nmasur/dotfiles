2848ae94240b3e9f38b3e63e6f99d4c908a77f5b
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-11-19 19:40:04 -0700

    switch kitty to victormono font

--- modules/darwin/fonts.nix
+++ modules/darwin/fonts.nix
@@ -3,13 +3,13 @@
   home-manager.users.${config.user} = {
 
     home.packages = with pkgs;
-      [ (nerdfonts.override { fonts = [ "Victor Mono" ]; }) ];
+      [ (nerdfonts.override { fonts = [ "VictorMono" ]; }) ];
 
-    programs.alacritty.settings = { font.normal.family = "Victor Mono"; };
+    programs.alacritty.settings = { font.normal.family = "VictorMono"; };
 
     programs.kitty.font = {
-      package = pkgs.nerdfonts;
-      name = "Victor Mono";
+      package = (pkgs.nerdfonts.override { fonts = [ "VictorMono" ]; });
+      name = "VictorMono Nerd Font Mono";
     };
 
   };
