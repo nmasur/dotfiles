7a39acef1a0b7367b0a518403987e661d46fcefc
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-11-28 22:36:30 -0700

    move some lua text to separate files

--- modules/neovim/plugins/lsp.nix
+++ modules/neovim/plugins/lsp.nix
@@ -51,46 +51,36 @@
   # };
 
   lua = ''
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-    vim.keymap.set("n", "gT", vim.lsp.buf.type_definition)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
-    vim.keymap.set("n", "gh", vim.lsp.buf.hover)
-    -- vim.keymap.set("n", "gr", telescope.lsp_references)
-    vim.keymap.set("n", "<Leader>R", vim.lsp.buf.rename)
-    vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
-    vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
-    vim.keymap.set("n", "<Leader>de", vim.diagnostic.open_float)
-    vim.keymap.set("n", "<Leader>E", vim.lsp.buf.code_action)
-
+    ${builtins.readFile ./lsp.lua}
 
     require("null-ls").setup({
         sources = {
-            require('null-ls').builtins.formatting.stylua.with({ command = "${pkgs.stylua}/bin/stylua" }),
-            require('null-ls').builtins.formatting.black.with({ command = "${pkgs.black}/bin/black" }),
-            require('null-ls').builtins.diagnostics.flake8.with({ command = "${pkgs.python310Packages.flake8}/bin/flake8" }),
-            require('null-ls').builtins.formatting.fish_indent.with({ command = "${pkgs.fish}/bin/fish_indent" }),
-            require('null-ls').builtins.formatting.nixfmt.with({ command = "${pkgs.nixfmt}/bin/nixfmt" }),
-            require('null-ls').builtins.formatting.rustfmt.with({ command = "${pkgs.rustfmt}/bin/rustfmt" }),
-            require('null-ls').builtins.diagnostics.shellcheck.with({ command = "${pkgs.shellcheck}/bin/shellcheck" }),
-            require('null-ls').builtins.formatting.shfmt.with({
+            require("null-ls").builtins.formatting.stylua.with({ command = "${pkgs.stylua}/bin/stylua" }),
+            require("null-ls").builtins.formatting.black.with({ command = "${pkgs.black}/bin/black" }),
+            require("null-ls").builtins.diagnostics.flake8.with({ command = "${pkgs.python310Packages.flake8}/bin/flake8" }),
+            require("null-ls").builtins.formatting.fish_indent.with({ command = "${pkgs.fish}/bin/fish_indent" }),
+            require("null-ls").builtins.formatting.nixfmt.with({ command = "${pkgs.nixfmt}/bin/nixfmt" }),
+            require("null-ls").builtins.formatting.rustfmt.with({ command = "${pkgs.rustfmt}/bin/rustfmt" }),
+            require("null-ls").builtins.diagnostics.shellcheck.with({ command = "${pkgs.shellcheck}/bin/shellcheck" }),
+            require("null-ls").builtins.formatting.shfmt.with({
                 command = "${pkgs.shfmt}/bin/shfmt",
-                extra_args = { '-i', '4', '-ci' },
+                extra_args = { "-i", "4", "-ci" },
             }),
-            require('null-ls').builtins.formatting.terraform_fmt.with({ command = "${pkgs.terraform}/bin/terraform" }),
+            require("null-ls").builtins.formatting.terraform_fmt.with({ command = "${pkgs.terraform}/bin/terraform" }),
         },
 
         on_attach = function(client, bufnr)
-              if client.supports_method("textDocument/formatting") then
-                  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                  vim.api.nvim_create_autocmd("BufWritePre", {
-                      group = augroup,
-                      buffer = bufnr,
-                      callback = function()
-                          vim.lsp.buf.format({ bufnr = bufnr })
-                      end,
-                  })
-              end
-        end
+            if client.supports_method("textDocument/formatting") then
+                vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = augroup,
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.format({ bufnr = bufnr })
+                    end,
+                })
+            end
+        end,
     })
   '';
 
