b258a40181cc5eaaa92098c02907e2903febe5c6
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-11-29 20:40:15 -0700

    working flake build of neovim

--- modules/neovim/default.nix
+++ modules/neovim/default.nix
@@ -1,29 +1,37 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
 
-  home-manager.users.${config.user} = {
+let
 
-    home.packages = [
-      (import ./package {
-        inherit pkgs;
-        colors = import config.theme.colors.neovimConfig { inherit pkgs; };
-      })
-    ];
-
-    programs.git.extraConfig.core.editor = "nvim";
-    home.sessionVariables = {
-      EDITOR = "nvim";
-      MANPAGER = "nvim +Man!";
-    };
-    programs.fish = {
-      shellAliases = { vim = "nvim"; };
-      shellAbbrs = {
-        v = lib.mkForce "nvim";
-        vl = lib.mkForce "vim -c 'normal! `0' -c 'bdelete 1'";
-        vll = "nvim -c 'Telescope oldfiles'";
-      };
-    };
-
+  neovim = import ./package {
+    inherit pkgs;
+    colors = import config.theme.colors.neovimConfig { inherit pkgs; };
   };
+
+in {
+
+  home-manager.users.${config.user} =
+
+    {
+
+      home.packages = [ neovim ];
+
+      programs.git.extraConfig.core.editor = "nvim";
+      home.sessionVariables = {
+        EDITOR = "nvim";
+        MANPAGER = "nvim +Man!";
+      };
+      programs.fish = {
+        shellAliases = { vim = "nvim"; };
+        shellAbbrs = {
+          v = lib.mkForce "nvim";
+          vl = lib.mkForce "nvim -c 'normal! `0' -c 'bdelete 1'";
+          vll = "nvim -c 'Telescope oldfiles'";
+        };
+      };
+      programs.kitty.settings.scrollback_pager = lib.mkForce ''
+        ${neovim}/bin/nvim -c 'setlocal nonumber nolist showtabline=0 foldcolumn=0|Man!' -c "autocmd VimEnter * normal G" -'';
+
+    };
 
   # # Used for icons in Vim
   # fonts.fonts = with pkgs; [ nerdfonts ];
