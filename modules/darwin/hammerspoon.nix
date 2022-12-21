1b3245795636aecdcd757b1ce982e83d896df54d
Noah Masur <7386960+nmasur@users.noreply.github.com>
2022-12-20 18:22:24 -0700

    fix: update dock for hammerspoon shortcuts

--- modules/darwin/hammerspoon.nix
+++ modules/darwin/hammerspoon.nix
@@ -1,9 +1,22 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
 
   # Hammerspoon - MacOS custom automation scripting
 
   home-manager.users.${config.user} = {
-    xdg.configFile.hammerspoon = { source = ./hammerspoon; };
+    xdg.configFile."hammerspoon/init.lua".source = ./hammerspoon/init.lua;
+    xdg.configFile."hammerspoon/Spoons/ControlEscape.spoon".source =
+      ./hammerspoon/Spoons/ControlEscape.spoon;
+    xdg.configFile."hammerspoon/Spoons/DismissAlerts.spoon".source =
+      ./hammerspoon/Spoons/DismissAlerts.spoon;
+    xdg.configFile."hammerspoon/Spoons/Launcher.spoon/init.lua".source =
+      pkgs.substituteAll {
+        src = ./hammerspoon/Spoons/Launcher.spoon/init.lua;
+        firefox = "${pkgs.firefox-bin}/Applications/Firefox.app";
+        discord = "${pkgs.discord}/Applications/Discord.app";
+        kitty = "${pkgs.kitty}/Applications/kitty.app";
+      };
+    xdg.configFile."hammerspoon/Spoons/MoveWindow.spoon".source =
+      ./hammerspoon/Spoons/MoveWindow.spoon;
   };
 
   homebrew.casks = [ "hammerspoon" ];
