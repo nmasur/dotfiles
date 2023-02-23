252b3d5dace14028a089a6971c7e15d9f0d0aabb
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-02-22 23:46:24 -0500

    temp: broken sleep attempt

--- modules/nixos/hardware/sleep.nix
+++ modules/nixos/hardware/sleep.nix
@@ -4,8 +4,16 @@
 
     # Prevent wake from keyboard
     powerManagement.powerDownCommands = ''
-      for wakeup in /sys/bus/usb/devices/1-*/power/wakeup; do echo disabled > $wakeup; done
+      # for power in /sys/bus/usb/devices/*/power; do echo disabled > ''${power}/wakeup; done
+
+      # AMD issue: https://wiki.archlinux.org/title/Power_management/Suspend_and_hibernate#Instantaneous_wakeups_from_suspend
+      for power in /sys/bus/i2c/devices/i2c-*/device/power; do echo disabled > ''${power}/wakeup; done
     '';
+
+    # From here: https://www.reddit.com/r/NixOS/comments/wcu34f/how_would_i_do_this_in_nix/
+    # services.udev.extraRules = ''
+    #   ACTION=="add", SUBSYSTEM=="i2c", ATTRS{idVendor}=="<vendor>", ATTRS{idProduct}=="<product>" RUN+="${pkgs.bash}/bin/bash -c 'echo disabled > /sys/bus/i2c/devices/i2c-*/power/wakeup'"
+    # '';
 
   };
 
