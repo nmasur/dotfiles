18c9e1efc8d1ff8e3d10f341f8be9fb27c7ade9a
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-02-25 09:22:16 -0500

    ssh settings for staff usb

--- generators/staff/default.nix
+++ generators/staff/default.nix
@@ -13,5 +13,23 @@ nixos-generators.nixosGenerate {
     users.extraUsers.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB+AbmjGEwITk5CK9y7+Rg27Fokgj9QEjgc9wST6MA3s"
     ];
+    services.openssh = {
+      enable = true;
+      ports = [ 22 ];
+      allowSFTP = true;
+      settings = {
+        GatewayPorts = "no";
+        X11Forwarding = false;
+        PasswordAuthentication = false;
+        PermitRootLogin = "yes";
+      };
+    };
+    environment.systemPackages =
+      with (import inputs.nixpkgs { inherit system; }); [
+        git
+        vim
+        wget
+        curl
+      ];
   }];
 }
