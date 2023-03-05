d46e6dad6b702f4bde13034e660e480d664254de
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-03-05 19:59:58 +0000

    open firewall for samba connection

--- modules/nixos/services/samba.nix
+++ modules/nixos/services/samba.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }: {
+{ config, pkgs, lib, ... }: {
 
   options = { samba.enable = lib.mkEnableOption "Enable Samba sharing."; };
 
@@ -6,6 +6,7 @@
 
     services.samba = lib.mkIf (config.samba.enable) {
       enable = true;
+      openFirewall = true;
       shares.video = {
         path = "/data/video";
         browseable = "yes";
@@ -20,6 +21,12 @@
     # Allow browsing Samba shares
     services.gvfs =
       lib.mkIf (config.gui.enable && config.nautilus.enable) { enable = true; };
+
+    # Permissions required to mount Samba with GVFS, if not using desktop environment
+    environment.systemPackages = lib.mkIf (config.gui.enable
+      && config.nautilus.enable
+      && config.services.xserver.windowManager.i3.enable)
+      [ pkgs.lxqt.pxqt-policykit ];
 
   };
 
