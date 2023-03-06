31170a616f4a32f2db3dc5e19ee90bbf726cc303
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-03-05 23:47:52 -0500

    client samba cleanup

--- modules/nixos/services/samba.nix
+++ modules/nixos/services/samba.nix
@@ -23,15 +23,15 @@
     networking.firewall.allowedTCPPorts = [ 5357 ];
     networking.firewall.allowedUDPPorts = [ 3702 ];
 
-    # Allow browsing Samba shares
+    # Allow client browsing Samba and virtual filesystem shares
     services.gvfs =
       lib.mkIf (config.gui.enable && config.nautilus.enable) { enable = true; };
 
-    # Permissions required to mount Samba with GVFS, if not using desktop environment
-    environment.systemPackages = lib.mkIf (config.gui.enable
-      && config.nautilus.enable
-      && config.services.xserver.windowManager.i3.enable)
-      [ pkgs.lxqt.pxqt-policykit ];
+    # # Permissions required to mount Samba with GVFS, if not using desktop environment
+    # environment.systemPackages = lib.mkIf (config.gui.enable
+    #   && config.nautilus.enable
+    #   && config.services.xserver.windowManager.i3.enable)
+    #   [ pkgs.lxqt.lxqt-policykit ];
 
   };
 
