a13083a264f3fa7ec9db415ff3e9be25a5aa82e8
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-04-02 08:22:28 -0400

    now really fix sleep

--- modules/nixos/hardware/sleep.nix
+++ modules/nixos/hardware/sleep.nix
@@ -7,13 +7,18 @@
       set +e
 
       for power in /sys/bus/i2c/devices/i2c-*/device/power; do echo disabled > ''${power}/wakeup || true; done
-      for power in /sys/bus/usb/devices/1-*/device/power; do echo disabled > ''${power}/wakeup || true; done
+      for power in /sys/bus/usb/devices/1-*/power; do echo disabled > ''${power}/wakeup || true; done
 
       # Fix for Gigabyte motherboard
       # /r/archlinux/comments/y7b97e/my_computer_wakes_up_immediately_after_i_suspend/isu99sr/
       echo GPP0 > /proc/acpi/wakeup
 
       set -e
+    '';
+
+    services.udev.extraRules = ''
+      ACTION=="add", SUBSYSTEM=="usb", DRIVER=="usb", ATTR{power/wakeup}="disabled"
+      ACTION=="add", SUBSYSTEM=="i2c", ATTR{power/wakeup}="disabled"
     '';
 
   };
