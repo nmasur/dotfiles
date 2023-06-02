ed19eaafd1673bc1574085d9ef55bef40ad32406
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-06-01 22:42:06 -0400

    auto wake and sleep for garbage collection
    only sleep again for non-servers

--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -11,6 +11,10 @@
     # This setting only applies to NixOS, different on Darwin
     nix.gc.dates = "weekly";
 
+    systemd.timers.nix-gc.timerConfig = { WakeSystem = true; };
+    systemd.services.nix-gc.postStop =
+      lib.mkIf (!config.server) "systemctl suspend";
+
   };
 
 }
