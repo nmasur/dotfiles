b282e76be4606d9f2fecc06d2dc8e58d5e3514be
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-06-24 15:15:50 -0600

    fix: gq with null-ls

--- modules/common/neovim/config/lsp.nix
+++ modules/common/neovim/config/lsp.nix
@@ -50,12 +50,19 @@
 
         on_attach = function(client, bufnr)
             if client.supports_method("textDocument/formatting") then
+                -- Auto-format on save
                 vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
                 vim.api.nvim_create_autocmd("BufWritePre", {
                     group = augroup,
                     buffer = bufnr,
                     callback = function()
                         vim.lsp.buf.format({ bufnr = bufnr })
+                    end,
+                })
+                -- Use internal formatting for bindings like gq.
+                vim.api.nvim_create_autocmd("LspAttach", {
+                    callback = function(args)
+                        vim.bo[args.buf].formatexpr = nil
                     end,
                 })
             end
