a46321202347a2e19da30a66f1d40e2ea2a83c78
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-07-09 14:51:41 -0600

    clarify neovimBuilder

--- modules/common/neovim/package/default.nix
+++ modules/common/neovim/package/default.nix
@@ -28,6 +28,8 @@
 
 { pkgs, colors, ... }:
 
+# Comes from nix2vim overlay:
+# https://github.com/gytis-ivaskevicius/nix2vim/blob/master/lib/neovim-builder.nix
 pkgs.neovimBuilder {
   package = pkgs.neovim-unwrapped;
   inherit colors;
