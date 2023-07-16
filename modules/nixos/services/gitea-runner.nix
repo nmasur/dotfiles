3cc264a857a957d2929187485997a8c77ff2783c
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-07-16 03:33:35 +0000

    fix: register gitea runner

--- modules/nixos/services/gitea-runner.nix
+++ modules/nixos/services/gitea-runner.nix
@@ -10,9 +10,9 @@
       enable = true;
       labels = [
         # Provide a Debian base with NodeJS for actions
-        "debian-latest:docker://node:18-bullseye"
+        # "debian-latest:docker://node:18-bullseye"
         # Fake the Ubuntu name, because Node provides no Ubuntu builds
-        "ubuntu-latest:docker://node:18-bullseye"
+        # "ubuntu-latest:docker://node:18-bullseye"
         # Provide native execution on the host using below packages
         "native:host"
       ];
@@ -29,6 +29,23 @@
       name = config.networking.hostName;
       url = "https://${config.hostnames.git}";
       tokenFile = config.secrets.giteaRunnerToken.dest;
+    };
+
+    secrets.giteaRunnerToken = {
+      source = ../../../private/gitea-runner-token.age; # TOKEN=xyz
+      dest = "${config.secretsDirectory}/gitea-runner-token";
+    };
+    systemd.services.giteaRunnerToken-secret = {
+      requiredBy = [
+        "gitea-runner-${
+          config.services.gitea-actions-runner.instances.${config.networking.hostName}.name
+        }.service"
+      ];
+      before = [
+        "gitea-runner-${
+          config.services.gitea-actions-runner.instances.${config.networking.hostName}.name
+        }.service"
+      ];
     };
 
   };
