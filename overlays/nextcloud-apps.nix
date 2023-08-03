25e3de5058e1accde39336cbcf594016cc8e916e
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-08-03 02:30:11 +0000

    fix: output hash for nextcloud apps

--- overlays/nextcloud-apps.nix
+++ overlays/nextcloud-apps.nix
@@ -3,15 +3,15 @@ inputs: _final: prev: {
   nextcloudApps = {
     news = prev.fetchNextcloudApp {
       url = inputs.nextcloud-news.outPath;
-      sha256 = inputs.nextcloud-news.sha256;
+      sha256 = inputs.nextcloud-news.narHash;
     };
     external = prev.fetchNextcloudApp {
       url = inputs.nextcloud-external.outPath;
-      sha256 = inputs.nextcloud-external.sha256;
+      sha256 = inputs.nextcloud-external.narHash;
     };
     cookbook = prev.fetchNextcloudApp {
       url = inputs.nextcloud-cookbook.outPath;
-      sha256 = inputs.nextcloud-cookbook.sha256;
+      sha256 = inputs.nextcloud-cookbook.narHash;
     };
   };
 
