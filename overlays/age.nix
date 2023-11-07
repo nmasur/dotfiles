6babfae211d4e576061a179da47b3ef7ce39d239
Noah Masur <7386960+nmasur@users.noreply.github.com>
2023-11-07 09:22:24 -0500

    fix: age build on x86-64_darwin fails

--- overlays/age.nix
+++ overlays/age.nix
@@ -1,5 +1,9 @@
 # Pin age because it is failing to build
+# https://github.com/NixOS/nixpkgs/pull/265753
 
 inputs: _final: prev: {
-  age = prev.age.overrideAttrs (old: { src = inputs.age; });
+  age = prev.age.overrideAttrs (old: {
+    src = inputs.age;
+    doCheck = false; # https://github.com/FiloSottile/age/issues/517
+  });
 }
