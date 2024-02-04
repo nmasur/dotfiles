865dad0f4f837791258633fd6beed6ad7f03af41
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-02-04 10:02:50 -0700

    Revert "remove ansible (no longer used)"
    This reverts commit 3178a1dea981a8b81b6d531a911cd36896c991c9.

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -8,7 +8,7 @@
       if ! xcode-select --version 2>/dev/null; then
         $DRY_RUN_CMD xcode-select --install
       fi
-      if ! /usr/local/bin/brew --version 2>/dev/null; then
+      if ! /opt/homebrew/bin/brew --version 2>/dev/null; then
         $DRY_RUN_CMD /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
       fi
     '';
