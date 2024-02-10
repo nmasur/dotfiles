2217568a59f295a4cd95da5a940652e88c919d58
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-02-10 09:18:37 -0500

    clean up commented xdg mime associations

--- modules/nixos/applications/nautilus.nix
+++ modules/nixos/applications/nautilus.nix
@@ -36,22 +36,11 @@
       # Set Nautilus as default for opening directories
       xdg.mimeApps = {
         associations.added."inode/directory" = [ "org.gnome.Nautilus.desktop" ];
-        # associations.removed = {
-        #   "inode/directory" = [ "kitty-open.desktop" ];
-        # };
         defaultApplications."inode/directory" =
           lib.mkBefore [ "org.gnome.Nautilus.desktop" ];
       };
 
     };
-
-    # # Set default for opening directories
-    # xdg.mime = {
-    #   addedAssociations."inode/directory" = [ "org.gnome.Nautilus.desktop" ];
-    #   removedAssociations = { "inode/directory" = [ "kitty-open.desktop" ]; };
-    #   defaultApplications."inode/directory" =
-    #     lib.mkForce [ "org.gnome.Nautilus.desktop" ];
-    # };
 
     # Delete Trash files older than 1 week
     systemd.user.services.empty-trash = {
