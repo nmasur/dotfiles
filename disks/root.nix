1e9401eef236461d482bce96f1723a7b0cb7d6bc
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-02-18 09:41:48 -0500

    update disko config to new format

--- disks/root.nix
+++ disks/root.nix
@@ -4,38 +4,30 @@
       type = "disk";
       device = disk;
       content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
+        type = "gpt";
+        partitions = {
           # Boot partition
-          {
-            name = "ESP";
-            start = "0";
-            end = "512MiB";
-            fs-type = "fat32";
-            bootable = true;
+          ESP = {
+            size = "512MiB";
+            type = "EF00";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
               extraArgs = [ "-n boot" ];
             };
-          }
+          };
           # Root partition ext4
-          {
-            name = "root";
-            start = "512MiB";
-            end = "100%";
-            part-type = "primary";
-            bootable = true;
+          root = {
+            size = "100%";
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
               extraArgs = [ "-L nixos" ];
             };
-          }
-        ];
+          };
+        };
       };
     };
   };
