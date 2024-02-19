4bfd65acb3df9cef5287aa2d8e16a5a422def8de
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-02-19 14:28:13 +0000

    fix: new disko format of disks wouldn't boot

--- disks/root.nix
+++ disks/root.nix
@@ -7,24 +7,28 @@
         type = "gpt";
         partitions = {
           # Boot partition
-          ESP = {
+          ESP = rec {
             size = "512MiB";
             type = "EF00";
+            label = "boot";
+            device = "/dev/disk/by-label/${label}";
             content = {
               type = "filesystem";
               format = "vfat";
               mountpoint = "/boot";
-              extraArgs = [ "-n boot" ];
+              extraArgs = [ "-n ${label}" ];
             };
           };
           # Root partition ext4
-          root = {
+          root = rec {
             size = "100%";
+            label = "nixos";
+            device = "/dev/disk/by-label/${label}";
             content = {
               type = "filesystem";
               format = "ext4";
               mountpoint = "/";
-              extraArgs = [ "-L nixos" ];
+              extraArgs = [ "-L ${label}" ];
             };
           };
         };
