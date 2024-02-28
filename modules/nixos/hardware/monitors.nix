bbdfeb645ecc96d7c0ef73b22ea42c57608dde0b
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-02-28 07:55:18 -0500

    kvm replaces hdmi monitor w displayport

--- modules/nixos/hardware/monitors.nix
+++ modules/nixos/hardware/monitors.nix
@@ -35,7 +35,7 @@
                                             --mode 1920x1200 \
                                             --pos 1920x0 \
                                             --rotate left \
-                                        --output HDMI-A-0 \
+                                        --output DisplayPort-0 \
                                             --primary \
                                             --mode 1920x1080 \
                                             --pos 0x560 \
