eb3c1f7a4c485a6b603b73542c9a3ca1fa6098ec
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-03-18 22:48:12 -0400

    Revert "kvm replaces hdmi monitor w displayport"
    This reverts commit bbdfeb645ecc96d7c0ef73b22ea42c57608dde0b.

--- modules/nixos/hardware/monitors.nix
+++ modules/nixos/hardware/monitors.nix
@@ -35,7 +35,7 @@
                                             --mode 1920x1200 \
                                             --pos 1920x0 \
                                             --rotate left \
-                                        --output DisplayPort-0 \
+                                        --output HDMI-A-0 \
                                             --primary \
                                             --mode 1920x1080 \
                                             --pos 0x560 \
