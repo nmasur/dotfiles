6b37ba13c98a42b4d23455e16c5e2c2f5dce6442
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-03-24 14:36:23 -0400

    tf init with definition in main

--- hosts/arrow/main.tf
+++ hosts/arrow/main.tf
@@ -1,5 +1,22 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket                      = "noahmasur-terraform"
+    key                         = "arrow.tfstate"
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+    /*
+      ENVIRONMENT VARIABLES
+      ---------------------
+      AWS_ACCESS_KEY_ID     - R2 token
+      AWS_SECRET_ACCESS_KEY - R2 secret
+      AWS_ENDPOINT_URL_S3   - R2 location: https://ACCOUNT_ID.r2.cloudflarestorage.com
+    */
+  }
   required_version = ">= 1.0.0"
   required_providers {
     aws = {
