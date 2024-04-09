3c2a971fc408adb39bb63e6798189a89112fa53a
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-04-09 17:06:13 -0400

    fix: arrow as host must include filesystems

--- hosts/arrow/default.nix
+++ hosts/arrow/default.nix
@@ -3,6 +3,18 @@
 inputs.nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   specialArgs = { };
-  modules = import ./modules.nix { inherit inputs globals overlays; };
+  modules = import ./modules.nix { inherit inputs globals overlays; } ++ [{
+    # This is the root filesystem containing NixOS
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+    };
+
+    # This is the boot filesystem for Grub
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
+  }];
 }
 
