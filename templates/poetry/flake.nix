0cdae3569e5e0b3756c34a24ec098eca821dc9fd
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-06-05 13:53:27 -0400

    remove flake-utils from poetry template

--- templates/poetry/flake.nix
+++ templates/poetry/flake.nix
@@ -1,34 +1,50 @@
 {
   description = "Python project flake";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+  };
   outputs =
+    { nixpkgs, poetry2nix, ... }:
+    let
+      projectDir = ./.;
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-      poetry2nix,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ poetry2nix.overlay ];
-        };
-        projectDir = ./.;
-      in
-      {
-        defaultPackage = pkgs.poetry2nix.mkPoetryApplication { inherit projectDir; };
-        devShells.default = pkgs.mkShell {
-          buildInputs = [
-            (pkgs.poetry2nix.mkPoetryEnv { inherit projectDir; })
-            pkgs.poetry
-          ];
-        };
-      }
-    );
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ poetry2nix.overlays.default ];
+          };
+        in
+        {
+          default = pkgs.poetry2nix.mkPoetryApplication { inherit projectDir; };
+        }
+      );
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ poetry2nix.overlays.default ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              (pkgs.poetry2nix.mkPoetryEnv { inherit projectDir; })
+              pkgs.poetry
+            ];
+          };
+        }
+      );
+    };
 }
