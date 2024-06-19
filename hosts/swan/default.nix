e5099431030211ad12429fe337ccfdf09f12c5a0
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-06-19 10:51:16 -0600

    add nixpkgs-caddy into swan

--- hosts/swan/default.nix
+++ hosts/swan/default.nix
@@ -8,9 +8,11 @@
   ...
 }:
 
-inputs.nixpkgs.lib.nixosSystem {
+inputs.nixpkgs.lib.nixosSystem rec {
   system = "x86_64-linux";
-  specialArgs = { };
+  specialArgs = {
+    pkgs-caddy = import inputs.nixpkgs-caddy { inherit system; };
+  };
   modules = [
     globals
     inputs.home-manager.nixosModules.home-manager
