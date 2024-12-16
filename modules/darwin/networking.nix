405b1ce60fe11195b0368297a3c02b4cd9970a82
Noah Masur <7386960+nmasur@users.noreply.github.com>
2024-12-16 14:08:29 -0500

    adjust lookingglass computer name

--- modules/darwin/networking.nix
+++ modules/darwin/networking.nix
@@ -6,11 +6,12 @@
 }:
 {
 
-  config = lib.mkIf pkgs.stdenv.isDarwin {
-    networking = {
-      computerName = config.networking.hostName;
-      # Adjust if necessary
-      # hostName = "";
-    };
-  };
+  #   config = lib.mkIf pkgs.stdenv.isDarwin {
+  #     networking = {
+  #       computerName = config.networking.hostName;
+  #       # Adjust if necessary
+  #       # hostName = "";
+  #     };
+  #   };
+  # }
 }
