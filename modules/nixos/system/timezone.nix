a4b5e05f8f78b9525e9dd7c2640172abe4b473dc
Noah Masur <7386960+nmasur@users.noreply.github.com>
2025-01-19 17:44:00 -0500

    switch from geoclue to tzupdate for localtime since geoclue isn't working

--- modules/nixos/system/timezone.nix
+++ modules/nixos/system/timezone.nix
@@ -8,22 +8,14 @@
 
   config = lib.mkIf pkgs.stdenv.isLinux {
 
+    services.tzupdate.enable = true;
+
     # Service to determine location for time zone
+    # This is required for redshift which depends on the location provider
     services.geoclue2.enable = true;
     services.geoclue2.enableWifi = false; # Breaks when it can't connect
     location = {
       provider = "geoclue2";
     };
-
-    # Enable local time based on time zone
-    services.localtimed.enable = true;
-
-    # Required to get localtimed to talk to geoclue2
-    services.geoclue2.appConfig.localtimed.isSystem = true;
-    services.geoclue2.appConfig.localtimed.isAllowed = true;
-
-    # Fix "Failed to set timezone"
-    # https://github.com/NixOS/nixpkgs/issues/68489#issuecomment-1484030107
-    services.geoclue2.enableDemoAgent = lib.mkForce true;
   };
 }
