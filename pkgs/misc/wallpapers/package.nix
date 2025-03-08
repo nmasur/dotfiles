dd95c94b6ea5c7911bc401fb3e1b89b76dbbe44a
Noah Masur <7386960+nmasur@users.noreply.github.com>
2025-03-07 19:59:44 -0500

    successful build

--- pkgs/misc/wallpapers/package.nix
+++ pkgs/misc/wallpapers/package.nix
@@ -7,7 +7,7 @@ pkgs.stdenv.mkDerivation {
     owner = "exorcist365";
     repo = "wallpapers";
     rev = "8d2860ac6c05cec0f78d5c9d07510f4ff5da90dc";
-    sha256 = "155lb7w563dk9kdn4752hl0zjhgnq3j4cvs9z98nb25k1xpmpki7";
+    sha256 = "sha256-1c1uDz37MhksWC75myv6jao5q2mIzD8X8I+TykXXmWg=";
   };
   installPhase = ''
     cp -r $src/ $out/
