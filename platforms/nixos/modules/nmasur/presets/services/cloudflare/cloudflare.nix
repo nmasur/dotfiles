ae3735586ee6c5b19ebebe094e17d7a1f5c91b1a
Noah Masur <7386960+nmasur@users.noreply.github.com>
2025-08-24 18:46:44 -0400

    fix: accidentally doubled cloudflare plugins

--- platforms/nixos/modules/nmasur/presets/services/cloudflare/cloudflare.nix
+++ platforms/nixos/modules/nmasur/presets/services/cloudflare/cloudflare.nix
@@ -67,8 +67,6 @@ in
 
     # Tell Caddy to use Cloudflare DNS for ACME challenge validation
     services.caddy.package = pkgs.caddy.withPlugins {
-      plugins = [ "github.com/caddy-dns/cloudflare@8cbec3f04d5b4a768c52941a5468c4b71436509e" ]; # v0.2.1
-      hash = "sha256-2D7dnG50CwtCho+U+iHmSj2w14zllQXPjmTHr6lJZ/A=";
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
       hash = "sha256-S1JN7brvH2KIu7DaDOH1zij3j8hWLLc0HdnUc+L89uU=";
     };
