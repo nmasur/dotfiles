d162df2515bd8331044da0215ee335010d3316f6
Noah Masur <7386960+nmasur@users.noreply.github.com>
2026-07-13 09:49:14 -0400

    use noti for gh run watch in zellij pane

--- platforms/home-manager/modules/nmasur/presets/programs/noti.nix
+++ platforms/home-manager/modules/nmasur/presets/programs/noti.nix
@@ -15,10 +15,21 @@ in
 
   config = lib.mkIf (cfg.enable && (pkgs.stdenv.isDarwin || config.services.dunst.enable)) {
     home.packages = [ pkgs.noti ];
-    programs.fish.shellAbbrs = {
-      # Add noti for ghpr in Darwin
-      ghpr = lib.mkForce "gh pr create && sleep 3 && noti gh run watch";
-      grw = lib.mkForce "noti gh run watch";
+    programs.fish = {
+      shellAbbrs = {
+        # Add noti for ghpr in Darwin
+        ghpr = lib.mkForce "gh pr create && sleep 3 && noti gh run watch";
+        grw = lib.mkForce "noti gh run watch";
+      };
+      functions = {
+        gh-run = {
+          body =
+            lib.mkForce # fish
+              ''
+                ${lib.getExe pkgs.zellij} action new-pane --start-suspended -- noti gh run watch
+              '';
+        };
+      };
     };
   };
 }
